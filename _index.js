b001790ddb901ae9cd76d67df59b9c21be5508b1
Holly <h.person.notrobot@gmail.com>
2022-11-16 20:37:11 +0000

    Removed changes of entries div, added entry id, added some future use functions

--- _index.js
+++ _index.js
@@ -7,15 +7,10 @@ console.log(Pokedex)
 window.toggle_sidebar = function (){
     if (document.getElementById("sidebar").style.width === defaultSidebarWidth){
         document.getElementById("sidebar").style.width = expandedSidebarWidth;
-        document.getElementById("entries").style.marginLeft = expandedSidebarWidth;
-        document.getElementById("entries").style.width = "75%";
     }else{
         document.getElementById("sidebar").style.width = defaultSidebarWidth;
-        document.getElementById("entries").style.marginLeft = defaultSidebarWidth;
-        document.getElementById("entries").style.width = "95%";
     }
 }
-
 
 function creatEntry(name,dex){
     let urlname = name.toLowerCase()
@@ -25,11 +20,11 @@ function creatEntry(name,dex){
     urlname.replaceAll(".", "")
     urlname.replaceAll("\u0301", "e")
 
-    let html = " <div class = \"entry\">\n"
-    html += "        <span class=\"link\"></span>\n"
-    html += "        <img src=\"https://img.pokemondb.net/sprites/sword-shield/icon/"+urlname+".png\" alt=\""+name+"\" class = \"pokemon_image\" height='100%'>\n"
-    html += "        <h1 class = \"pokemon_title\">"+(name.charAt(0).toUpperCase()+name.slice(1))+"</h1>\n"
-    html += "        <h2 class=\"pokemon_dexno\">#"+dex+"</h2>\n"
+    let html = " <div class = 'entry' id='urlname' onclick='pokemon_click("+urlname+")'>\n"
+    html += "        <span class='link'></span>\n"
+    html += "        <img src='https://img.pokemondb.net/sprites/sword-shield/icon/"+urlname+".png' alt='"+name+"' class = 'pokemon_image' height='100%'>\n"
+    html += "        <h1 class = 'pokemon_title'>"+(name.charAt(0).toUpperCase()+name.slice(1))+"</h1>\n"
+    html += "        <h2 class='pokemon_dexno'>#"+dex+"</h2>\n"
     html += "    </div>"
     const template = document.createElement("template");
     template.innerHTML = html.trim();
